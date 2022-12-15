4775c1253f945e2ba1a530003698fc95d71bf193
ArceDaniel <jonathandanielarce9@gmail.com>
2022-12-15 00:08:16 -0300

    details page now is responsive

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -24,15 +24,15 @@ export default function Details(){
     console.log(character)
     return (
       <div className={style.card}>
-        <div>
+        <div  className={style.imgContainer}>
           <img src={character.image} alt={character.name} className={style.img} />
         </div>
         <div className={style.descrition}>
           <h2>{character.name}</h2>   
-          <h2>Origen: {character.origin?.name}</h2>
-          <h2>Especie: {character.species}</h2>
-          <h2>Estado: {character.status}</h2>
-          <h2>Genero: {character.gender}</h2>
+          <h2><span style={{color:' #ae00ff'}}>Origen: </span>{character.origin?.name}</h2>
+          <h2><span style={{color:' #ae00ff'}}>Especie:  </span>{character.species}</h2>
+          <h2><span style={{color:' #ae00ff'}}>Estado:  </span>{character.status}</h2>
+          <h2><span style={{color:' #ae00ff'}}>Genero:  </span>{character.gender}</h2>
         </div>
       </div>
     )
